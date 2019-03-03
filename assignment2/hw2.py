2e755c32dd780a209aa057ca43878c34ed9c7e82
tessa mcneil <tnmcneil@bu.edu>
2019-03-02 19:20:31 -0500

    update

--- assignment2/hw2.py
+++ assignment2/hw2.py
@@ -6,7 +6,6 @@ import random
 import numpy as np
 import math
 
-
 def read_in_shakespeare():
     """
     Reads in the Shakespeare dataset processes it into a list of tuples.
@@ -88,6 +87,12 @@ def create_term_document_matrix(line_tuples, document_names, vocab):
     # print(td_matrix[(vocab_to_id['soldier'])][28])
     # print(td_matrix[(vocab_to_id['soldier'])][18])
     # print(td_matrix[(vocab_to_id['soldier'])][26])
+    # juliet = vocab_to_id['juliet']
+    # romeo = vocab_to_id['romeo']
+    # print("term doc")
+    # print(td_matrix[juliet])
+    # print(td_matrix[romeo])
+    # print(np.sum(td_matrix[vocab_to_id['love']]))
 
     return td_matrix
 
@@ -114,17 +119,37 @@ def create_term_context_matrix(line_tuples, vocab, context_window_size=1):
     m = len(vocab)
     tc_matrix = np.zeros((m,m))
 
-    for i in range(len(line_tuples)):
+    for i in range(0,len(line_tuples)):
         line = line_tuples[i][1]
-        for j in range(len(line)):
+        for j in range(0,len(line)):
             t_id = vocab_to_id[line[j]]
             lb = max(0, j-context_window_size)
-            ub = min(len(line)-1, j+context_window_size)
+            ub = min(len(line), j+context_window_size)
             context = line[lb:ub+1]
-            for k in range(len(context)):
+            for k in range(0,len(context)):
+                # print("target word j:")
+                # print(line[j])
+                # print("context word i:")
+                # print(context[k])
                 c_id = vocab_to_id[context[k]]
                 tc_matrix[t_id][c_id] += 1
-    print(tc_matrix)
+                # print("co-occurrence of words " + str(t_id) + " and " + str(+ c_id))
+                # print(tc_matrix[t_id][c_id])
+    #
+    # juliet = vocab_to_id['juliet']
+    # romeo = vocab_to_id['romeo']
+    # love = vocab_to_id['love']
+    # print("Term context")
+    # print(tc_matrix[juliet][juliet])
+    # print(tc_matrix[romeo][romeo])
+    # print(tc_matrix[love][love])
+    # print(tc_matrix[juliet][love])
+    # print(max(tc_matrix[0][1:]))
+    # print(vocab_to_id)
+    # for a in range(len(tc_matrix)):
+    #     print(max(tc_matrix[a]))
+    #     print(tc_matrix[a][a])
+    #     print(tc_matrix[a].sort()[-2])
 
     return tc_matrix
 
@@ -143,8 +168,33 @@ def create_PPMI_matrix(term_context_matrix):
        and the jth word in the term_context_matrix.
     """
 
-    # YOUR CODE HERE
-    return None
+    n = term_context_matrix.shape[0]
+    # words = 0
+    # for i in range(n):
+    #     words += term_context_matrix[i][i]
+    # frequency_matrix = np.divide(term_context_matrix, words)
+
+    ppmi_matrix = np.zeros((n,n))
+
+    # print(term_context_matrix)
+
+    for i in range(n):
+        for j in range(n):
+            # print('ij')
+            # print(term_context_matrix[i][j])
+            # print('ii')
+            # print(term_context_matrix[i][i])
+            # print('jj')
+            # print(term_context_matrix[j][j])
+            if term_context_matrix[i][j] == 0:
+                val = 0
+            else:
+                val = math.log(term_context_matrix[i][j] / (term_context_matrix[i][i]*term_context_matrix[j][j]))
+            ppmi_matrix[i][j] = val
+            # ppmi_matrix[i][j] = math.log((term_context_matrix[i][j]/(term_context_matrix[i][i]*term_context_matrix[j][j])))
+
+    # print(ppmi_matrix)
+    return ppmi_matrix
 
 def create_tf_idf_matrix(term_document_matrix):
     """
@@ -176,8 +226,8 @@ def compute_cosine_similarity(vector1, vector2):
       A scalar similarity value.
     """
     dp = np.dot(vector1, vector2)
-    mag1 = math.sqrt(sum(i**2 for i in vector1))
-    mag2 = math.sqrt(sum(i**2 for i in vector2))
+    mag1 = np.sqrt(vector1.dot(vector1))
+    mag2 = np.sqrt(vector2.dot(vector2))
     similarity = dp/(mag1*mag2)
     return similarity
 
@@ -192,11 +242,10 @@ def compute_jaccard_similarity(vector1, vector2):
     Returns:
       A scalar similarity value.
     """
-
-    intersect = set(vector1.flatten()).intersection(set(vector2.flatten()))
-    union = set(vector1.flatten()).union(set(vector2.flatten()))
-    similarity = len(intersect)/len(union)
-    # ALSO CHECK: len(intersection) / (len(a) + len(b) - len(intersection))
+    dp = np.dot(vector1, vector2)
+    c1 = vector1.dot(vector1)
+    c2 = vector2.dot(vector2)
+    similarity = dp / (c1 + c2 - dp)
     return similarity
 
 def compute_dice_similarity(vector1, vector2):
@@ -210,11 +259,10 @@ def compute_dice_similarity(vector1, vector2):
     Returns:
       A scalar similarity value.
     """
-    # IMPLEMENTED SET VERSION
-    # CHECK TP/FN VERSION: 2TP/(2TP + FP + FN)
-    # ALSO CHECK 2|A DOT B| / |A|^2 + |B|^2  (NEED BINARY VECTORS)
-    intersect = set(vector1.flatten()).intersection(set(vector2.flatten()))
-    similarity = 2*len(intersect)/(len(vector1) + len(vector2))
+    dp = np.dot(vector1, vector2)
+    c1 = vector1.dot(vector1)
+    c2 = vector2.dot(vector2)
+    similarity = 2*dp / (c1 + c2)
     return similarity
 
 def rank_plays(target_play_index, term_document_matrix, similarity_fn):
@@ -237,11 +285,14 @@ def rank_plays(target_play_index, term_document_matrix, similarity_fn):
     target_play_vector = term_document_matrix[:,target_play_index]
 
     for i in range(term_document_matrix.shape[1]):
-        comparison_play_vector = term_document_matrix[:,i]
-        similarity = similarity_fn(target_play_vector, comparison_play_vector)
-        similarities[i] = similarity
+        if i != target_play_index:
+            comparison_play_vector = term_document_matrix[:,i]
+            similarity = similarity_fn(target_play_vector, comparison_play_vector)
+            similarities[i] = similarity
 
-    return sorted(similarities, key=similarities.get)
+    # print(similarities)
+    # print(sorted(similarities, key=similarities.get))
+    return sorted(similarities, key=similarities.get, reverse=True)
 
 def rank_words(target_word_index, matrix, similarity_fn):
     """
@@ -259,15 +310,19 @@ def rank_words(target_word_index, matrix, similarity_fn):
       target word indexed by word_index
     """
     similarities = dict()
-
-    target_word_vector = matrix[target_word_index]
+    print('matrix')
+    print(matrix)
+    print('target index is: ' + str(target_word_index))
+    target_word_vector = matrix[target_word_index,:]
 
     for i in range(matrix.shape[0]):
-        comparison_word_vector = matrix[i]
-        similarity = similarity_fn(target_word_vector, comparison_word_vector)
-        similarities[i] = similarity
+        if i != target_word_index:
+            comparison_word_vector = matrix[i,:]
+            similarity = similarity_fn(target_word_vector, comparison_word_vector)
+            similarities[i] = similarity
 
-    return sorted(similarities, key=similarities.get)
+    # print(similarities)
+    return sorted(similarities, key=similarities.get, reverse=True)
 
 
 if __name__ == '__main__':
@@ -276,42 +331,40 @@ if __name__ == '__main__':
     print('Computing term document matrix...')
     td_matrix = create_term_document_matrix(tuples, document_names, vocab)
 
-    # print('Computing tf-idf matrix...')
-    # tf_idf_matrix = create_tf_idf_matrix(td_matrix)
+    print('Computing tf-idf matrix...')
+    tf_idf_matrix = create_tf_idf_matrix(td_matrix)
 
     print('Computing term context matrix...')
     tc_matrix = create_term_context_matrix(tuples, vocab, context_window_size=2)
 
-    # print('Computing PPMI matrix...')
-    # PPMI_matrix = create_PPMI_matrix(tc_matrix)
+    print('Computing PPMI matrix...')
+    PPMI_matrix = create_PPMI_matrix(tc_matrix)
+    print(PPMI_matrix.dtype)
 
     random_idx = random.randint(0, len(document_names)-1)
     similarity_fns = [compute_cosine_similarity, compute_jaccard_similarity, compute_dice_similarity]
 
-    # for sim_fn in similarity_fns:
-    #     print('\nThe top most similar plays to "%s" using %s are:' % (document_names[random_idx], sim_fn.__qualname__))
-    #     ranks = rank_plays(random_idx, td_matrix, sim_fn)
-    #     for idx in range(0, 1):
-    #         doc_id = ranks[idx]
-    #         print('%d: %s' % (idx+1, document_names[doc_id]))
+    for sim_fn in similarity_fns:
+        print('\nThe top most similar plays to "%s" using %s are:' % (document_names[random_idx], sim_fn.__qualname__))
+        ranks = rank_plays(random_idx, td_matrix, sim_fn)
+        for idx in range(0, 1):
+            doc_id = ranks[idx]
+            print('%d: %s' % (idx+1, document_names[doc_id]))
 
-    # word = 'juliet'
-    # vocab_to_index = dict(zip(vocab, range(0, len(vocab))))
-    # for sim_fn in similarity_fns:
-    #     print('\nThe 10 most similar words to "%s" using %s on term-document frequency matrix are:' % (word, sim_fn.__qualname__))
-    #     ranks = rank_words(vocab_to_index[word], td_matrix, sim_fn)
-    #     for idx in range(0, 10):
-    #         word_id = ranks[idx]
-    #         print('%d: %s' % (idx+1, vocab[word_id]))
+    word = 'juliet'
+    vocab_to_index = dict(zip(vocab, range(0, len(vocab))))
+    for sim_fn in similarity_fns:
+        print('\nThe 10 most similar words to "%s" using %s on term-document frequency matrix are:' % (word, sim_fn.__qualname__))
+        ranks = rank_words(vocab_to_index[word], td_matrix, sim_fn)
+        for idx in range(0, 10):
+            word_id = ranks[idx]
+            print('%d: %s' % (idx+1, vocab[word_id]))
 
     word = 'juliet'
     vocab_to_index = dict(zip(vocab, range(0, len(vocab))))
     for sim_fn in similarity_fns:
         print('\nThe 10 most similar words to "%s" using %s on term-context frequency matrix are:' % (word, sim_fn.__qualname__))
-        print("pre calc")
         ranks = rank_words(vocab_to_index[word], tc_matrix, sim_fn)
-        print("ranks")
-        print(ranks)
         for idx in range(0, 10):
             word_id = ranks[idx]
             print('%d: %s' % (idx+1, vocab[word_id]))
